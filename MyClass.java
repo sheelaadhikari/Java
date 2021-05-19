66b8135a55dac99d6b49c662888b0f9b197d7524
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2021-05-19 13:29:51 +0545

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		
+		System.out.print("hello world");
+
+	}
+
+}
