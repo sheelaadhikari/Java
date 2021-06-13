ada7bf48620c6eda8eef30757db232e164f64c49
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2021-06-13 08:21:40 +0545

    Add files via upload

--- IdentifierClass.java
+++ IdentifierClass.java
@@ -1,6 +1,6 @@
 
 
-public class IdentifierTest {
+public class IdentifierClass {
 
  public static void main(String[] args) {
 float f = 30.45f; // float contains value after decimal
@@ -8,8 +8,8 @@ int i = (int)f; //explicit type casting
 
 int a = 100;
 float b = a; //automatic type casting
-System.out.println(a);
-System.out.println(i);
+System.out.println(+a);
+System.out.println(+i);
 
 
  }
