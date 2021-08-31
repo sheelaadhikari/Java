ba219d3b779e61cc8dcf733d7839f657a901adf0
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2021-08-31 16:14:02 +0545

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,20 @@
+
+public class Recursion {
+	
+	
+	public static void main(String[] args) {
+		int a= 5;
+		int i;
+		i=a+5;
+		
+
+
+		
+		System.out.println("the value of i is " +i);
+		}
+	}
+
+
+
+
+
