e3d4e55734f10bfa67f80f387a7e7cbb3826b8e6
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2021-09-15 16:35:05 +0545

    Add files via upload

---
+++ Greater.java
@@ -0,0 +1,16 @@
+package firstsession;
+
+public class Greater {
+	
+	public static void main( String args[]){
+		int a=98;
+		int b=23;
+		if(a>b) {
+		System.out.println("a is greater than b");
+		
+		}else {
+			System.out.println(" b is greater than a");
+		}
+	}
+	
+}
