2bca22ac67be40231ba29b0062013d00dc0564d2
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2021-09-27 16:02:48 +0545

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,18 @@
+package firstsession;
+
+public class Example {
+	
+	public int areaOfRectangle(int l, int b) {
+		return l*b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Example area = new Example();
+		int a =area.areaOfRectangle(23, 34);
+	System.out.println(+a);
+	
+		
+	}
+
+}
