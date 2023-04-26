43bf8df9f1c7abb5aabc9a747ad95fab72093a24
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2023-04-26 16:58:17 +0545

    Add files via upload

--- CookiesExamplr.java
+++ CookiesExamplr.java
@@ -1,12 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.net.*;
+import java.io.*;
 
-/**
- *
- * @author HP
- */
 public class CookiesExamplr {
-    
+  public static void main(String[] args) {
+    try {
+      URL url = new URL("https://www.example.com");
+
+      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+      connection.setRequestMethod("GET");
+
+      int responseCode = connection.getResponseCode();
+      System.out.println("Response Code: " + responseCode);
+
+      String cookieHeader = connection.getHeaderField("Set-Cookie");
+      System.out.println("Cookies: " + cookieHeader);
+
+      CookieManager cookieManager = new CookieManager();
+      CookieHandler.setDefault(cookieManager);
+      HttpCookie cookie = HttpCookie.parse(cookieHeader).get(0);
+      cookieManager.getCookieStore().add(null, cookie);
+
+      HttpURLConnection connection2 = (HttpURLConnection) url.openConnection();
+      connection2.setRequestProperty("Cookie", cookieManager.getCookieStore().getCookies().get(0).toString());
+      int responseCode2 = connection2.getResponseCode();
+      System.out.println("Response Code with Cookies: " + responseCode2);
+    } catch (Exception e) {
+      System.out.println("Exception: " + e);
+    }
+  }
 }
